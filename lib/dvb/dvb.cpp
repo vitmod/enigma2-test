d01bdfdff87dd9a7ccb3615b4a6b387a974ba1ed
Christian Ege <k4230r6@gmail.com>
2015-06-11 21:00:02 +0200

    Added Amlogic specific decoder for enigma2
    Signed-off-by: Christian Ege <k4230r6@gmail.com>

--- lib/dvb/dvb.cpp
+++ lib/dvb/dvb.cpp
@@ -156,6 +156,8 @@ eDVBResourceManager::eDVBResourceManager()
 		m_boxtype = DM800;
 	else if (!strncmp(tmp, "ebox7358\n", rd))
 		m_boxtype = DM800SE;
+	else if (!strncmp(tmp, "wetekplay\n", rd))
+		m_boxtype = WETEKPLAY;
 	else {
 		eDebug("boxtype detection via /proc/stb/info not possible... use fallback via demux count!\n");
 		if (m_demux.size() == 3)
