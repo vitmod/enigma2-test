15222442957ccf583a06d169fefd54d538d93a1c
Christian Ege <k4230r6@gmail.com>
2015-06-11 21:04:32 +0200

    Fixed possible exceptions due to incomplete e2procfs
    Thx to delfi
    
    Signed-off-by: Christian Ege <k4230r6@gmail.com>

--- lib/python/Components/AVSwitch.py
+++ lib/python/Components/AVSwitch.py
@@ -152,9 +152,13 @@ class AVSwitch:
 
 		mode_etc = None
 		if os.path.exists('/proc/stb/video/videomode_50hz') and getBoxType() not in ('gb800solo', 'gb800se', 'gb800ue'):
-			f = open("/proc/stb/video/videomode_50hz", "w")
-			f.write(mode_50)
-			f.close()
+			try:
+				f = open("/proc/stb/video/videomode_50hz", "w")
+				f.write(mode_50)
+				f.close()
+			except IOError:
+				print "setting videomode_50hz failed."
+
 		if os.path.exists('/proc/stb/video/videomode_60hz') and getBoxType() not in ('gb800solo', 'gb800se', 'gb800ue'):
 			try:
 				f = open("/proc/stb/video/videomode_60hz", "w")
@@ -273,9 +277,12 @@ class AVSwitch:
 
 	def setAspect(self, cfgelement):
 		print "[VideoMode] setting aspect: %s" % cfgelement.value
-		f = open("/proc/stb/video/aspect", "w")
-		f.write(cfgelement.value)
-		f.close()
+		try:
+			f = open("/proc/stb/video/aspect", "w")
+			f.write(cfgelement.value)
+			f.close()
+		except IOError:
+			print "setting aspect failed."
 
 	def setWss(self, cfgelement):
 		if not cfgelement.value:
@@ -290,9 +297,12 @@ class AVSwitch:
 
 	def setPolicy43(self, cfgelement):
 		print "[VideoMode] setting policy: %s" % cfgelement.value
-		f = open("/proc/stb/video/policy", "w")
-		f.write(cfgelement.value)
-		f.close()
+		try:
+			f = open("/proc/stb/video/policy", "w")
+			f.write(cfgelement.value)
+			f.close()
+		except IOError:
+			print "setting policy43 failed."
 
 	def setPolicy169(self, cfgelement):
 		if os.path.exists("/proc/stb/video/policy2"):
